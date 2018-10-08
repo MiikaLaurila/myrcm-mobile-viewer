ecfcf992282a5d6c7b6d2b9abaa1af8514df2abe
Miika Laurila <laurila.miika@gmail.com>
2018-10-08 18:51:34 +0300

    Fix?

---
+++ scripts/minify.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd js/
+java -jar ../minifiers/closure.jar --js_output_file=myrcm-mobile-min.js '**.js' '!**-min.js' --create_source_map '%outname%.map'
+
+echo "" >> myrcm-mobile-min.js
+echo "//# sourceMappingURL=../js/myrcm-mobile-min.js.map" >> myrcm-mobile-min.js
+
+cd ..
+cd style/
+
+rm styles.css
+cat reset.css index.css > styles.css 
